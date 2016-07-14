17d3425b2b5a617287bc469862b2b856f8f54ac3
Ömer Cinbat <ocinbat@outlook.com>
2016-07-14 11:31:54 +0300

    Created Trendyol.Excelsior.MultipartDataMediaFormatter package for MultipartDataMediaFormatter support.

---
+++ Trendyol.Excelsior.MultipartDataMediaFormatter/ExcelsiorExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MultipartDataMediaFormatter.Infrastructure;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace Trendyol.Excelsior.MultipartDataMediaFormatter
+{
+    public static class ExcelsiorExtensions
+    {
+        public static IEnumerable<T> Listify<T>(this IExcelsior excelsior, HttpFile httpFile, bool hasHeaderRow = false)
+        {
+            string fileExtension = Path.GetExtension(httpFile.FileName) ?? String.Empty;
+
+            IWorkbook workbook;
+
+            using (Stream stream = new MemoryStream(httpFile.Buffer))
+            {
+                switch (fileExtension.ToLower())
+                {
+                    case ".xls":
+                        workbook = new HSSFWorkbook(stream);
+                        break;
+                    case ".xlsx":
+                        workbook = new XSSFWorkbook(stream);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Excelsior can only operate on .xsl and .xlsx files.");
+                }
+            }
+
+            return excelsior.Listify<T>(workbook, hasHeaderRow);
+        }
+
+        public static IEnumerable<T> Listify<T>(this IExcelsior excelsior, HttpFile httpFile, IRowValidator<T> rowValidator, out IEnumerable<T> invalids, bool hasHeaderRow = false)
+        {
+            string fileExtension = Path.GetExtension(httpFile.FileName) ?? String.Empty;
+
+            IWorkbook workbook;
+
+            using (Stream stream = new MemoryStream(httpFile.Buffer))
+            {
+                switch (fileExtension.ToLower())
+                {
+                    case ".xls":
+                        workbook = new HSSFWorkbook(stream);
+                        break;
+                    case ".xlsx":
+                        workbook = new XSSFWorkbook(stream);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Excelsior can only operate on .xsl and .xlsx files.");
+                }
+            }
+
+            return excelsior.Listify<T>(workbook, rowValidator, out invalids, hasHeaderRow);
+        }
+
+        public static IEnumerable<string[]> Listify(this IExcelsior excelsior, HttpFile httpFile, bool hasHeaderRow = false)
+        {
+            string fileExtension = Path.GetExtension(httpFile.FileName) ?? String.Empty;
+
+            IWorkbook workbook;
+
+            using (Stream stream = new MemoryStream(httpFile.Buffer))
+            {
+                switch (fileExtension.ToLower())
+                {
+                    case ".xls":
+                        workbook = new HSSFWorkbook(stream);
+                        break;
+                    case ".xlsx":
+                        workbook = new XSSFWorkbook(stream);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Excelsior can only operate on .xsl and .xlsx files.");
+                }
+            }
+
+            return excelsior.Listify(workbook, hasHeaderRow);
+        }
+    }
+}
